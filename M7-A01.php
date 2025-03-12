eab767d9de4fddeb83948976e04ce94aae3d1bfc
Martí Vilàs <150129703+MartiVilas@users.noreply.github.com>
2025-03-12 09:37:15 +0000

    finish exercicis inicials

---
+++ M7-A01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicis inicials</title>
+</head>
+<body>
+    <ol>
+        <li><a href="exercici1.php">Exercici 1 </a></li>
+        <li><a href="exercici2.php">Exercici 2 </a></li>
+        <li><a href="exercici3.php">Exercici 3 </a></li>
+        <li><a href="exercici4.php">Exercici 4 </a></li>
+    </ol>
+</body>
+</html>
