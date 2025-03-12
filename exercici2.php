9d1ce4641a3405159fd95234f6c45e6915cd84dd
Martí Vilàs <150129703+MartiVilas@users.noreply.github.com>
2025-03-12 09:45:28 +0000

    finish ex2 with modifications

--- exercici2.php
+++ exercici2.php
@@ -1,7 +1,12 @@
 <?php
 
 for ($i=100; $i >= 0 ; $i--) { 
-    echo("$i <br>");
+    if($i%2==0){
+        echo("<strong> $i </strong> <br>");
+    }else if($i%3==0){
+        echo("<em> $i </em> <br>");
+    }else if($i == 0){
+        echo("<strong><em>$i</strong></em>");
+    }
 }
-
 ?>
