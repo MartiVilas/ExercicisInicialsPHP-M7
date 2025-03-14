692c75035539ab9737ab8c930b237aba57eb52a5
Martí Vilàs <150129703+MartiVilas@users.noreply.github.com>
2025-03-14 07:32:33 +0000

    fix ex2 in class

--- exercici2.php
+++ exercici2.php
@@ -6,7 +6,9 @@ for ($i=100; $i >= 0 ; $i--) {
     }else if($i%3==0){
         echo("<em> $i </em> <br>");
     }else if($i == 0){
-        echo("<strong><em>$i</strong></em>");
+        echo("<strong><em>$i</em></strong><br>");
+    }else {
+        echo("$i <br>");
     }
 }
 ?>
