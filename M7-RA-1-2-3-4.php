de3a541b52b339ad6f553c2c815b7909b4768455
Martí Vilàs <150129703+MartiVilas@users.noreply.github.com>
2025-03-14 08:33:58 +0000

    create A02 and redo index structure

---
+++ M7-RA-1-2-3-4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicis inicials M7-A01</title>
+</head>
+<body>
+    <h1>A01 - Exercici inicials</h1>
+    <ol> 
+        <li><a href="A01/exercici1.php">Exercici 1 </a></li>
+        <li><a href="A01/exercici2.php">Exercici 2 </a></li>
+        <li><a href="A01/exercici3.php">Exercici 3 </a></li>
+        <li><a href="A01/exercici4.php">Exercici 4 </a></li>
+    </ol>
+    <h1>A02 - Estadístiques Bàsiques</h1>
+    <ol>
+        <li><a href="A02/exercici1.php">Exercici 1</a></li>
+        <li><a href="A02/exercici2.php">Exercici 2</a></li>
+    </ol>
+</body>
+</html>
